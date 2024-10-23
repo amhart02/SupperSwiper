67b2b28e75e59e1bb8dc3ed1fbbe49e52e5a685b
brycewoodland <bwoodlan@byui.edu>
2024-10-23 17:27:35 -0600

    GETs for all restaurants and by ID

--- backend/config/database.js
+++ backend/config/database.js
@@ -28,4 +28,4 @@ async function connectDB() {
   }
 }
 
-module.exports = { connectDB };
+module.exports = { connectDB, supabase };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { connectDB } = require('./config/database');
+const restaurantRoutes = require('./routes/restaurantRoutes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./swaggerConfig');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,7 +11,8 @@ const port = process.env.PORT || 8080;
 connectDB();
 
 app.use(bodyParser.json());
-
+app.use('/', restaurantRoutes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);

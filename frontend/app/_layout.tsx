96467efa23fd570d2c5b17db430eb1a898d29be7
amhart02 <har20010@byui.edu>
2024-10-23 16:23:56 -0600

    Additional changes to the screen setup

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,11 +3,11 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack initialRouteName="index">
-      <Stack.Screen name="index" />
-      <Stack.Screen name="connection" />
-      <Stack.Screen name="restaurants"/>
-      <Stack.Screen name="match"/>
-      <Stack.Screen name="results"/>
+      <Stack.Screen name="index" options={{ headerShown: false}} />
+      <Stack.Screen name="connection" options={{ headerShown: false}}/>
+      <Stack.Screen name="restaurants" options={{ headerShown: false}}/>
+      <Stack.Screen name="match" options={{ headerShown: false}}/>
+      <Stack.Screen name="results" options={{ headerShown: false}}/>
     </Stack>
   );
 }
